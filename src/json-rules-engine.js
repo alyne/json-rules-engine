0cc46c1a01f6976ba761e05dbf6ea992fc8edcbc
Manuel Reil <manuel.reil@alyne.com>
2025-08-17 10:24:13 +0200

    Harmonizes import

--- src/json-rules-engine.js
+++ src/json-rules-engine.js
@@ -5,7 +5,28 @@ import Operator from './operator'
 import Almanac from './almanac'
 import OperatorDecorator from './operator-decorator'
 
-export { Fact, Rule, Operator, Engine, Almanac, OperatorDecorator }
+// Import sync variants
+import EngineSync from './sync/engine-sync'
+import FactSync from './sync/fact-sync'
+import RuleSync from './sync/rule-sync'
+import AlmanacSync from './sync/almanac-sync'
+import ConditionSync from './sync/condition-sync'
+
+export { 
+  Fact, 
+  Rule, 
+  Operator, 
+  Engine, 
+  Almanac, 
+  OperatorDecorator,
+  // Sync variants
+  EngineSync,
+  FactSync,
+  RuleSync,
+  AlmanacSync,
+  ConditionSync
+}
+
 export default function (rules, options) {
   return new Engine(rules, options)
 }
